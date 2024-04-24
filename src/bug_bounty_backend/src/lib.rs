be40a8737b5b4cb7a19198e2b1a7cc59b6af53c9
marvincd <mharvehll@gmail.com>
2024-04-24 21:28:32 +0100

    first commit

--- src/bug_bounty_backend/src/lib.rs
+++ src/bug_bounty_backend/src/lib.rs
@@ -1,4 +1,215 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize, Principal};
+
+use serde::Serialize;
+use ic_cdk::{ post_upgrade, pre_upgrade, query, update, init, storage,};
+use std::cell::RefCell;
+use std::collections::{BTreeMap, BTreeSet};
+
+mod models;
+use crate::{models::*};
+
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
+use crate::models::{BountyAccount, UserProfile};
+
+type IdStore = BTreeMap<String, Principal>;
+type ProfileStore = BTreeMap<Principal, UserProfile>;
+type TournamentStore = BTreeMap<String, BountyAccount>;
+
+thread_local! {
+    static PROFILE_STORE: RefCell<ProfileStore> = RefCell::default();
+    static BOUNTY_STORE: RefCell<TournamentStore> = RefCell::default();
+    static ID_STORE: RefCell<IdStore> = RefCell::default();
 }
+
+#[query(name = "getSelf")]
+fn get_self(principal:Principal) -> UserProfile {
+    // let id = ic_cdk::api::caller();
+    PROFILE_STORE.with(|profile_store| {
+        profile_store
+            .borrow()
+            .get(&principal)
+            .cloned().unwrap_or_default()
+    })
+}
+
+#[query]
+fn get_all_user() -> Vec<UserProfile> {
+    PROFILE_STORE.with(|profile_store| {
+        let mut all_users = Vec::new();
+        profile_store.borrow().iter().for_each(|user| {
+            all_users.push((*user.1).clone().try_into().unwrap_or_default())
+        });
+        all_users
+    })
+}
+
+#[query]
+fn get_profile(name: String) -> UserProfile {
+    ID_STORE.with(|id_store| {
+        PROFILE_STORE.with(|profile_store| {
+            id_store
+                .borrow()
+                .get(&name)
+                .and_then(|id| profile_store.borrow().get(id).cloned()).unwrap_or_default()
+        })
+    })
+}
+
+#[update]
+fn create_profile(profile: UserProfile,principal:Principal) -> Result<u8,u8> {
+    // let principal_id = ic_cdk::api::caller();
+    ID_STORE.with(|id_store| {
+        id_store
+            .borrow_mut()
+            .insert(profile.username.clone(), principal);
+    });
+    PROFILE_STORE.with(|profile_store| {
+        profile_store.borrow_mut().insert(principal, profile);
+    });
+    Ok(1)
+}
+
+#[query]
+fn get_bounty(id: String) -> BountyAccount {
+    BOUNTY_STORE.with(|bounty_store| {
+        bounty_store.borrow().get(&id).cloned().unwrap_or_default()
+    })
+}
+
+#[query]
+fn get_all_bounty() -> Vec<BountyAccount> {
+    BOUNTY_STORE.with(|bounty_store| {
+        let mut all_bounty = Vec::new();
+        bounty_store.borrow().iter().for_each(|bounty| {
+            all_bounty.push((*bounty.1).clone().try_into().unwrap_or_default())
+        });
+        all_bounty
+    })
+}
+
+#[update]
+fn create_bounty(bounty: BountyAccount) ->  Result<u8,u8>{
+    let id_hash = bounty.clone().id_hash;
+
+    BOUNTY_STORE.with(|bounty_store| {
+        bounty_store.borrow_mut().insert(id_hash, bounty);
+    });
+    Ok(1)
+}
+
+#[update]
+fn start_bounty(id: String) {
+    BOUNTY_STORE.with(|bounty_store| {
+        let mut bounty = bounty_store.borrow().get(&id).cloned().unwrap_or_default();
+        bounty.status = match bounty.status {
+            BountyStatus::AcceptingHunters => BountyStatus::BountyHuntingInProgress,
+            _ => {
+                BountyStatus::BountyHuntingInProgress
+            }
+        };
+        bounty_store.borrow_mut().insert(id, bounty);
+    });
+}
+
+#[update]
+fn end_bounty(id: String, names:Vec<String>,principal:Principal) {
+    if get_self(principal).is_mod {
+        BOUNTY_STORE.with(|bounty_store| {
+            let mut bounty = bounty_store.borrow().get(&id).cloned().unwrap_or_default();
+            bounty.status = match bounty.status {
+                BountyStatus::AcceptingHunters => BountyStatus::BountyHuntingInProgress,
+                _ => {
+                    BountyStatus::BountyHuntingInProgress
+                }
+            };
+
+            names.iter().for_each(|name| {
+                bounty.winers.push(name.try_into().unwrap());
+            });
+            bounty_store.borrow_mut().insert(id, bounty);
+        });
+    }
+    else {
+        println!("you're not admin");
+    }
+}
+
+#[update]
+fn join_bounty(name: String,id: String) {
+    BOUNTY_STORE.with(|bounty_store| {
+        let mut bounty = bounty_store.borrow().get(&id).cloned().unwrap_or_default();
+        bounty.user.push(name);
+        bounty_store.borrow_mut().insert(id, bounty);
+    });
+}
+
+#[update]
+fn set_mod(name: String, identity: Principal) {
+    ID_STORE.with(|id_store| {
+        PROFILE_STORE.with(|profile_store| {
+            let mut user = id_store
+                .borrow()
+                .get(&name)
+                .and_then(|id| profile_store.borrow().get(id).cloned()).unwrap_or_default();
+            user.is_mod = true;
+            profile_store.borrow_mut().insert(identity, user);
+        })
+    });
+}
+
+#[query]
+fn is_mod(name: String) -> bool {
+    ID_STORE.with(|id_store| {
+        PROFILE_STORE.with(|profile_store| {
+            let mut user = id_store
+                .borrow()
+                .get(&name)
+                .and_then(|id| profile_store.borrow().get(id).cloned()).unwrap_or_default();
+            user.is_mod
+        })
+    })
+}
+
+
+// // Retrieves the value associated with the given key if it exists.
+// #[ic_cdk_macros::query]
+// fn get_stable_profile(key: Principal) -> Option<UserProfile> {
+//     MAP.with(|p| p.borrow().get(&key))
+// }
+//
+// // Inserts an entry into the map and returns the previous value of the key if it exists.
+// #[ic_cdk_macros::update]
+// fn insert_stable_profile(key: Principal, value: UserProfile) -> Option<UserProfile> {
+//     MAP.with(|p| p.borrow_mut().insert(key, value))
+// }
+//
+// // Retrieves the value associated with the given key if it exists.
+// #[ic_cdk_macros::query]
+// fn get_stable_bounty(key: String) -> Option<BountyAccount> {
+//     BOUNTY_MAP.with(|p| p.borrow().get(&key))
+// }
+//
+// // Inserts an entry into the map and returns the previous value of the key if it exists.
+// #[ic_cdk_macros::update]
+// fn insert_stable_bounty(key: String, value: BountyAccount) -> Option<BountyAccount> {
+//     BOUNTY_MAP.with(|p| p.borrow_mut().insert(key, value))
+// }
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    PROFILE_STORE.with(|users| storage::stable_save((users,)).unwrap());
+    BOUNTY_STORE.with(|bountys| storage::stable_save((bountys,)).unwrap());
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    let (old_users,): ( ProfileStore,) = storage::stable_restore().unwrap();
+    PROFILE_STORE.with(|users| *users.borrow_mut() = old_users);
+    let (old_bountys,): ( TournamentStore,) = storage::stable_restore().expect("");
+    BOUNTY_STORE.with(|bountys| *bountys.borrow_mut() = old_bountys);
+}
+
+
+// Enable Candid export
+ic_cdk::export_candid!();
---
+++ src/bug_bounty_backend/src/models.rs
@@ -0,0 +1,247 @@
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{
+    storable::Bound, DefaultMemoryImpl, StableBTreeMap, Storable,
+};
+use std::{borrow::Cow, cell::RefCell};
+use crate::*;
+use ic_cdk::print;
+use candid::{decode_one, encode_one};
+use ic_cdk::{api::time};
+use serde::{Serialize};
+
+use ic_websocket_cdk::{
+    send, ClientPrincipal, OnCloseCallbackArgs, OnMessageCallbackArgs, OnOpenCallbackArgs,
+};
+
+#[derive(CandidType, Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
+pub struct AppMessage {
+    pub text: String,
+    pub timestamp: u64,
+}
+
+impl AppMessage {
+    fn candid_serialize(&self) -> Vec<u8> {
+        encode_one(&self).unwrap()
+    }
+}
+
+pub fn on_open(args: OnOpenCallbackArgs) {
+    let msg = AppMessage {
+        text: String::from("ping"),
+        timestamp: time(),
+    };
+    send_app_message(args.client_principal, msg);
+}
+
+pub fn on_message(args: OnMessageCallbackArgs) {
+    let app_msg: AppMessage = decode_one(&args.message).unwrap();
+    let new_msg = AppMessage {
+        text: String::from("ping"),
+        timestamp: time(),
+    };
+    print(format!("Received message: {:?}", app_msg));
+    send_app_message(args.client_principal, new_msg)
+}
+
+fn send_app_message(client_principal: ClientPrincipal, msg: AppMessage) {
+    print(format!("Sending message: {:?}", msg));
+    if let Err(e) = send(client_principal, msg.candid_serialize()) {
+        println!("Could not send message: {}", e);
+    }
+}
+
+pub fn on_close(args: OnCloseCallbackArgs) {
+    print(format!("Client {} disconnected", args.client_principal));
+}
+
+const MAX_VALUE_SIZE: u32 = 100;
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub struct UserProfile {
+    pub id_hash: String,
+    pub age: u8,
+    pub date: String,
+    pub status: Status,
+    pub wins: u8,
+    pub bountys_created:u8,
+    pub points: Option<u128>,
+    pub username: String,
+    pub is_mod: bool,
+    pub principal_id: String,
+    pub account_id : String,
+    pub canister_id: String,
+    pub guild_badge: String,
+}
+
+
+// For a type to be used in a `StableBTreeMap`, it needs to implement the `Storable`
+// trait, which specifies how the type can be serialized/deserialized.
+//
+// In this example, we're using candid to serialize/deserialize the struct, but you
+// can use anything as long as you're maintaining backward-compatibility. The
+// backward-compatibility allows you to change your struct over time (e.g. adding
+// new fields).
+//
+// The `Storable` trait is already implemented for several common types (e.g. u64),
+// so you can use those directly without implementing the `Storable` trait for them.
+impl Storable for UserProfile {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+
+    const BOUND: Bound = Bound::Bounded {
+        max_size: MAX_VALUE_SIZE,
+        is_fixed_size: false,
+    };
+}
+
+#[derive(Clone,Debug, Default, CandidType, Deserialize, Serialize)]
+pub struct BountyAccount {
+    pub id_hash: String,
+    pub creator: String,
+    pub creator_id: Option<String>,
+    pub status: BountyStatus,
+    pub idx: u8,
+    pub starting_date: String,
+    pub bounty_rules: String,
+    pub bounty_type: BountyType,
+    pub game: String,
+    pub guild:Vec<Guild>,
+    pub guild_points: Option<Vec<(String,u128)>>,
+    pub guild_in_game_names:Option<Vec<(String, String)>>,
+    pub messages: Option<Vec<Chat>>,
+    pub user: Vec<String>,
+    pub winers: Vec<String>,
+    pub entry_prize: u8,
+    pub total_prize: u128,
+    pub no_of_winners: u8,
+    pub no_of_participants: u128,
+    pub game_type: GameType,
+    pub end_date: String,
+    pub title: String,
+    pub in_game_names: Option<Vec<(String,String)>>,
+    pub points: Option<Vec<(String,u128)>>,
+    pub milestones: Option<Vec<MilestonesAccount>>
+}
+
+#[derive(Clone,Debug, Default, CandidType, Deserialize, Serialize)]
+pub struct MilestonesAccount {
+    pub status: BountyStatus,
+    pub milestone_status: MilestonesStatus,
+    pub idx: u8,
+    pub starting_date: Option<String>,
+    pub milestone_rules: String,
+    pub bounty_type: BountyType,
+    pub game: String,
+    pub guilds:Vec<Guild>,
+    pub messages: Option<Vec<Chat>>,
+    pub participants: Vec<String>,
+    pub winers: Vec<String>,
+    pub no_of_winners: Option<u8>,
+    pub no_of_participants: u128,
+    pub game_type: GameType,
+    pub name: Option<String>,
+}
+
+impl Storable for BountyAccount {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+
+    const BOUND: Bound = Bound::Bounded {
+        max_size: MAX_VALUE_SIZE,
+        is_fixed_size: false,
+    };
+}
+
+// #[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+// pub struct NewBountyAccount{
+//     pub oldbountys : BountyAccount,
+//     pub guild:  Vec<Guild>,
+// }
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub enum Status {
+    #[default]
+    Online,
+    Offline,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub enum GameType {
+    #[default]
+    TeamvTeam,
+    Single,
+    Duo,
+    Guild
+}
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub enum BountyStatus {
+    #[default]
+    AcceptingHunters,
+    BountyHuntingInProgress,
+    BountyHuntingCompleted,
+    Archived,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub enum MilestonesStatus {
+    #[default]
+    readyToStart,
+    GameInProgress,
+    GameCompleted,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub enum BountyType {
+    #[default]
+    OpenSource,
+    Reproduced,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, Serialize)]
+pub struct TokenState {
+    pub bump: u8,
+    pub amount: u64,
+}
+
+#[derive(Clone, Debug,  PartialEq, Default, Ord, Eq, PartialOrd, CandidType, Deserialize, Serialize)]
+pub enum GuildType {
+    #[default]
+    Open,
+    Closed,
+}
+
+#[derive(Clone, Debug, PartialEq, Default, Ord, Eq, PartialOrd, CandidType, Deserialize, Serialize)]
+pub struct Guild {
+    pub id_hash: String,
+    pub captain: String,
+    pub status: GuildType,
+    pub name: String,
+    pub tag: String,
+    pub members: Vec<Member>,
+    pub requests: Vec<String>,
+    pub points: Option<u128>,
+}
+
+#[derive(Clone, Debug, Default, PartialEq, Ord, Eq, PartialOrd, CandidType, Deserialize, Serialize)]
+pub struct Member {
+    pub name: String,
+    pub principal_id: String,
+}
+
+#[derive(Clone, Debug, Default, PartialEq, Ord, Eq, PartialOrd, CandidType, Deserialize, Serialize)]
+pub struct Chat {
+    pub name: String,
+    pub id: String,
+    pub time: String,
+    pub message: String,
+}
